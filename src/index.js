e4a92076538fd33fd365c25ddde1aafa7881aa9b
gvemulapalli33 <gvemulapalli33@gmail.com>
2020-12-30 19:01:40 -0800

    update router

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {WeatherContextProvider} from "./context/weatherContext";
+import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
+   <Router basename={window.location.pathname || ''}>
     <WeatherContextProvider>
       <App />
     </WeatherContextProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
